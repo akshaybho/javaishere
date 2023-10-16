18d976ae83ffac44a0fbe1992abb8ba9b34e052c
akshaybho <bhogale.akshay11@gmail.com>
2023-10-16 07:53:05 +0530

    Changes added

--- src/main/java/practice_questions/Prime.java
+++ src/main/java/practice_questions/Prime.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Prime {
     public void prime(){
-        //int count;
+      int temp = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a Number upto which you required prime numbers");
         int n = sc.nextInt();
         for(int i=2; i<=n; i++) {
-           int count = 0;
+            int count = 0;
             for (int j = 1; j <= i; j++) {
 
                 if (i % j == 0) {
@@ -17,10 +17,13 @@ public class Prime {
                 }
             }
             if (count == 2) {
-
+              temp = temp + 1;
                 System.out.print(i + " ");
+
         }
-        }
+
+        }System.out.println();
+        System.out.println("Total prime numbers upto "+n+" = "+temp);
         }
         public static void main(String[] args) {
         Prime p = new Prime();
