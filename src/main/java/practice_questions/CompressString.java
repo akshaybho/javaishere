fbb66b6da654aac5842c1a3172e9dc9c157fc6af
akshaybho <bhogale.akshay11@gmail.com>
2023-10-24 20:27:56 +0530

    compress the String

---
+++ src/main/java/practice_questions/CompressString.java
@@ -0,0 +1,28 @@
+package practice_questions;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class CompressString {
+
+    public static void main(String[] args) {
+
+        String str = "aaabbbccdddd";
+        Map <Character, Integer> map = new LinkedHashMap<>();
+        for(int i=0; i<str.length();i++){
+            if(map.get(str.charAt(i))!=null){
+                map.put(str.charAt(i), map.get(str.charAt(i))+1);
+
+            }
+            else{
+                map.put(str.charAt(i),1);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+       for(Map.Entry <Character, Integer> entry : map.entrySet()){
+               sb.append(entry.getKey());
+               sb.append(entry.getValue());
+       }
+        System.out.println(sb.toString());
+    }
+}
